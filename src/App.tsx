b876b7041326ab62637e3324ae15e3b28f872ff7
Ricardo Mejias Travieso <63120247+Ricardo-MT@users.noreply.github.com>
2022-12-23 12:14:49 +0100

    Added Press Start 2P font

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
-import axios from 'axios';
-import React, { FormEvent, useState } from 'react';
-
-const login = ({email, password}:{email: string, password: string}) => axios.post(process.env.REACT_APP_API_URL + "/authentication", {email, password})
-
 const App = () => {
-  const [state, setState] = useState<string | null>(null);
-
-  const handleLogin = (event : FormEvent<HTMLFormElement>) => {
-    event.stopPropagation()
-    event.preventDefault()
-    const email = event.currentTarget.email.value
-    const password = event.currentTarget.password.value
-    login({
-      email,
-      password,
-    }).then(res=>{
-      setState(res.data.user.name)
-    }).catch(err=>{
-      console.log(err)
-      setState("ERROR")
-    })
-  }
-
   return (
-    <div className="App">
-      {state? <p>User logged: {state}</p> : <p>No user logged</p>}
-      <form onSubmit={handleLogin}>
-        <p>
-          <input name='email' type="email" />
-        </p>
-        <p>
-          <input name='password' type="password" />
-        </p>
-          <input type="submit" />
-      </form>
+    <div style={{
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    }} >
+      <h1>GDG ALGECIRAS</h1>
+      <h2>web en construcción</h2>
     </div>
   );
 }
